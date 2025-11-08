d023a5f10e1de51e1bbfe22de2ffc78ab9cf6806
Parvin Mohammadghasemi <pamohamm@f0r1s2.codam.nl>
2025-11-08 20:32:09 +0100

    ft_printf.pdf: added

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamohamm <pamohamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 16:56:15 by pamohamm          #+#    #+#             */
+/*   Updated: 2025/11/08 20:13:43 by pamohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+int	ft_printf(const	char *, ...);
+
+#endif
