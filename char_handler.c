7810038d698bac76d199a086dbee4ab33657da92
parvinghasemi <mghasemi.parvin@gmail.com>
2025-11-11 21:34:39 +0100

    char_handler.c: added print_pointer()

--- char_handler.c
+++ char_handler.c
@@ -22,3 +22,22 @@ int	print_string(char *str)
 // 	printf("%s\n", str);
 // 	print_string(str);
 // }
+
+int print_pointer(void *ptr)
+{
+	unsigned long address;
+	int		printed;
+
+	printed = 0;
+	write(1, "0x", 2);
+	printed += 2;
+	return (printed);
+}
+
+// int main()
+// {
+// 	void *p = NULL;
+// 	printf("%p\n", p);
+// 	printf("%p\n", NULL);
+// 	print_pointer(p);
+// }
